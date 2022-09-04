8b3f3abdd6496db895e1578ac65f0977edb8d2d9
laurette <dalmorolaurette@orange.fr>
2022-09-04 19:39:35 +0200

    essai affichage données api (echec)

--- frontend_sportsee/src/services/fetch/fetchApi.js
+++ frontend_sportsee/src/services/fetch/fetchApi.js
@@ -1,11 +1,19 @@
+// eslint-disable-next-line no-template-curly-in-string
 const url = "http://localhost:3000/user";
+console.log (url)
 
 const getData = async (id, categorie) => {
     let urlDemande = categorie ? url + `/${id}/${categorie}` : url+ `/${id}`
+    console.log (urlDemande)
 
     const data = await fetch(urlDemande)
+    console.log (data)
+
     const dataFetch = await data.json()
+    console.log(dataFetch)
+
     return dataFetch
 }
+console.log (getData)
 
 export default getData
